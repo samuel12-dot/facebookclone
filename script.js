8b23fce136d8a50951151ff90d05d85689e91823
samuel12-dot <samuelogunduyile76@gmail.com>
2025-08-10 19:03:46 +0100

    feat: add toggle button

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let settingsMenu = document.querySelector(".settings")
+let settingsMenu = document.querySelector(".settings-menu")
 
 function settingsMenuToggle(){
     settingsMenu.classList.toggle("settings-menu-height")
